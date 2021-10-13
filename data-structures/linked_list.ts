d6112b76f54b7faa9882d35d41d95a6a2fcccf56
sangramabdar <abdarsangram2697@gmail.com>
2021-10-13 14:15:05 +0530

    reduced time complexity of indexing operation from n to n/2 and added error handling

--- data-structures/linked_list.ts
+++ data-structures/linked_list.ts
@@ -33,7 +33,6 @@ class LinkedList<T> {
     }
 
     let tempNode: LinkedListNode<T> | null = this.tail!!;
-    let previousNode: LinkedListNode<T> | null = this.tail.prevoius;
 
     tempNode.next = new LinkedListNode<T>(this.legnth, value);
     tempNode.next.prevoius = tempNode;
@@ -98,6 +97,8 @@ class LinkedList<T> {
   }
 
   insert(index: number, value: T) {
+    this.showError();
+
     if (this.isValidIndex(index)) {
       return;
     }
@@ -156,6 +157,9 @@ class LinkedList<T> {
       console.log(
         `${tempNode?.index} => ${tempNode?.prevoius?.index} ${tempNode?.next?.index}`
       );
+      if (i == this.legnth - 2) {
+        break;
+      }
       tempNode = tempNode!!.next;
     }
   }
@@ -164,22 +168,47 @@ class LinkedList<T> {
     let tempNode: LinkedListNode<T> | null = this.head!!;
     for (let i = 0; i < this.legnth; i++) {
       console.log(`${tempNode?.index} => ${tempNode?.value}`);
+      if (i == this.legnth - 2) {
+        break;
+      }
       tempNode = tempNode!!.next;
     }
   }
 
-  private indexing() {
-    let tempNode: LinkedListNode<T> | null = this.head;
-    for (let i = 0; i < this.legnth; i++) {
-      if (!tempNode) {
+  showError() {
+    if (this.legnth < 2) {
+      throw new Error(
+        "plz add minimum two elements by using add method , then perform this operation"
+      );
+    }
+  }
+
+  indexing() {
+    this.showError();
+
+    let length = this.legnth / 2;
+    let indexSting = `${length}`;
+
+    length = parseInt(indexSting);
+
+    let currentNode: LinkedListNode<T> | null = this.head;
+    let nextNode: LinkedListNode<T> | null = this.head?.next!!;
+    let index = 0;
+    for (let i = 0; i < length; i++) {
+      currentNode!!.index = index;
+      nextNode!!.index = index + 1;
+
+      //event number operation
+      if (this.legnth % 2 == 0) {
         break;
       }
-      if (i > 0) {
-        tempNode!!.index = i;
-        tempNode.prevoius!!.index = i - 1;
-      }
-      tempNode = tempNode!!.next;
+
+      currentNode = nextNode?.next!!;
+      nextNode = nextNode!!.next!!.next;
+      index = index + 2;
     }
+
+    this.tail!!.index = this.legnth - 1;
   }
 
   deleteTail() {
@@ -187,10 +216,10 @@ class LinkedList<T> {
   }
 
   delete(index: number) {
+    this.showError();
     if (this.isValidIndex(index)) {
       return;
     }
-
     let tempNode: LinkedListNode<T> | null = null;
     let previousNode: LinkedListNode<T> | null = null;
 
--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
 import LinkedList from "./data-structures/linked_list";
 
-let l = new LinkedList<string>();
-l.add("sam");
-l.add("ganesh");
-l.add("chirag");
-l.add("vicky");
-l.insert(3, "sangram");
-l.insert(2, "akshay");
-// l.insert(3, "sddsdasd");
-// l.delete(0);
-l.delete(0);
-// l.print();
-console.log();
-l.printNodes();
+let l = new LinkedList<number>();
+for (let i = 0; i < 3; i++) {
+  l.add(i);
+}
+
+l.indexing();
+l.delete(1);
+l.insert(0, 2);
+
+l.print();
