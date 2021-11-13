e49f37fe3cd5704bc2d496d854d2ebec7319e1b1
sangramabdar <abdarsangram2697@gmail.com>
2021-11-13 15:03:33 +0530

    quick sort added

---
+++ algorithms/quickSort.ts
@@ -0,0 +1,20 @@
+function QuickSort(array: number[]): number[] {
+  if (array.length < 2) {
+    return array;
+  } else {
+    let pivot = array[0];
+    let less = [];
+    let greater = [];
+
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] <= pivot) {
+        less.push(array[i]);
+      } else {
+        greater.push(array[i]);
+      }
+    }
+    return [...QuickSort(less), pivot, ...QuickSort(greater)];
+  }
+}
+
+console.log(QuickSort([1, -2, 1100, 22, -22]));
