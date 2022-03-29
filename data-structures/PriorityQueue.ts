1061c8b898f002a062cbd91c099e7b611730457f
sangramabdar <abdarsangram2697@gmail.com>
2022-03-29 14:29:08 +0530

    priority queue with stack is added

--- data-structures/PriorityQueue.ts
+++ data-structures/PriorityQueue.ts
@@ -1,17 +1,28 @@
 import { MaxHeap } from "./Heap";
+import Stack from "./Stack";
+
+class PriorityQueueNode<T> {
+  value: T;
+  priority: number;
+  constructor(value: T, priority: number) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
 
 class PriorityQueue<T> {
-  #maxHeap: MaxHeap<T> = new MaxHeap();
+  #maxHeap: MaxHeap<PriorityQueueNode<T>> = new MaxHeap();
 
-  dequeue(): T | null {
+  dequeue(): PriorityQueueNode<T> | null {
     return this.#maxHeap.delete();
   }
 
-  enqueue(value: T) {
-    this.#maxHeap.add(value);
+  enqueue(value: T, priority: number) {
+    let node = new PriorityQueueNode<T>(value, priority);
+    this.#maxHeap.add(node);
   }
 
-  peek(): T | null {
+  peek(): PriorityQueueNode<T> | null {
     return this.#maxHeap.peek();
   }
 
@@ -20,4 +31,41 @@ class PriorityQueue<T> {
   }
 }
 
-export default PriorityQueue;
+class PriorityQueueWithStack<T> {
+  values: Stack<PriorityQueueNode<T>> = new Stack<PriorityQueueNode<T>>();
+
+  //o(n) time complexity
+  enqueue(value: T, priority) {
+    let node = new PriorityQueueNode<T>(value, priority);
+    this.#enqueue(node);
+  }
+
+  #enqueue(node: PriorityQueueNode<T>) {
+    if (this.values.isEmpty) {
+      return this.values.push(node);
+    }
+
+    let currentNode = this.values.pop();
+
+    if (currentNode!!.priority <= node.priority) {
+      this.values.push(currentNode!!);
+      return this.values.push(node);
+    }
+
+    this.#enqueue(node);
+    this.values.push(currentNode!!);
+  }
+
+  //o(1) time complexity
+  dequeue(): T | null {
+    if (this.values.isEmpty) return null;
+    return this.values.pop()?.value!!;
+  }
+
+  isEmpty(): boolean {
+    if (this.values.isEmpty) return true;
+    return false;
+  }
+}
+
+export { PriorityQueue, PriorityQueueWithStack };
